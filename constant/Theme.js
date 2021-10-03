630318d092ffc64713afc99f7c2d75e91f504fe6
sharma-bhuvnesh <bhuvneshpssharma@gmail.com>
2021-10-03 18:30:12 +0530

    structure update

---
+++ constant/Theme.js
@@ -0,0 +1,5 @@
+export const PrimaryColor = '#ff6265';
+export const WhiteColor = '#ffffff';
+export const TextColor = '#4a4a4a';
+export const LightTextColor = '#9b9b9b';
+export const BorderColor= '#f1f1f1';
